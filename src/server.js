a86b2081da293e84e2a9fa45134037b13e4c0cd1
Younes <younes.benaomar@gmail.com>
2022-12-01 17:46:48 +0100

    fix port

--- src/server.js
+++ src/server.js
@@ -5,7 +5,7 @@ const dotenv = require("dotenv");
 dotenv.config();
 
 const app = express();
-const port = process.env.SERVER_PORT || 3000;
+const port = process.env.PORT || 3000;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
